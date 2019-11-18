09b10b934ad12510795554d8d91a107a3932ad3d
pineapple79 <fun.goul.109@gmail.com>
2019-11-18 15:50:13 +0600

    Task2-22

---
+++ numm22.py
@@ -0,0 +1,7 @@
+celsius = 90
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+# fahrenheit = 13.5
+# celsius = (fahrenheit - 32)/1.8
+# print('%0.1f fahrenheit degree to %0.1f celsius degree'%(fahrenheit, celsius))
